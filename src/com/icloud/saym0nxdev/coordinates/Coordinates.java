ed1be83a64a080cad5012633c05cad7b604a325d
Saymon Artemiev <saym0nxdev@icloud.com>
2024-02-26 23:00:59 +0100

    feat: Added Coordinates homework

---
+++ src/com/icloud/saym0nxdev/coordinates/Coordinates.java
@@ -0,0 +1,11 @@
+package com.icloud.saym0nxdev.coordinates;
+
+public class Coordinates {
+    public static void main(String[] args) {
+        double latitude = 38.8709889;
+        double longitude = -77.0559611;
+
+        System.out.println("Latitude: " + latitude);
+        System.out.println("Longitude: " + longitude);
+    }
+}
